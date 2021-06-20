3f8af81c2ba579e81dd6d24895d53cd5fafb33c2
guillerbe <gbenlezar@orange.fr>
2021-06-20 13:13:51 +0200

    test finalisation branche dev

--- README.md
+++ README.md
@@ -1,36 +1,101 @@
-# Archive_old_files
-Ce projet s'inscrit dans le cadre de mon cursus de formation d'AIC dispensé par OC. 
-Il constitue ma première expérience Python, Ansible, Git / GitHub, partage de code avec la communaute en open source. [...] 
+# Module Ansible Archive_old_files
+Ce projet de création de module Ansible en Python s'inscrit dans le cadre de mon cursus de formation d'AIC dispensé par OC. 
+Il constitue ma première expérience Python et Ansible, Git / GitHub et partage de code avec la communauté en open source. 
 
 ## Description :
-Contexte du "lab" :
-La quantité de données stockées sur les serveurs de fichiers de plusieurs clients est en croissance élevée. Une phase intermediaire de traitement visant à atténuer l'augmentation rapide et importante de la taille des espaces de stockage est proposée. 
+Contexte du "lab" : La quantité de données stockées sur les serveurs de fichiers de plusieurs clients est en croissance élevée. En tant que prestataire informatique nous proposons une phase intermediaire de traitement avec pour objectif de réduire la taille des données dans ces espaces de stockage. 
 
-Le projet Archive_old_files est un des outils utilisé dans cette étape intermédiaire. Ce projet, initié sous Python 3.9, a pour but d'automatiser l'archivage / compression de fichiers dont la date de dernière consultation est considérée comme trop ancienne. 
-Le fichier original après archivage / compression est supprimé de l'espace de stockage. 
+Le projet de module Ansible "Archive_old_files" est l'un  des outils utilisé dans cette phase intermédiaire. Ce module, initié sous Python 3.9, sert à automatiser l'archivage / compression des fichiers dont la date de dernière consultation est considérée comme trop ancienne par les clients. 
+Les fichiers originaux après archivage / compression au format zip sont supprimés de l'espace de stockage. Les archives sont laissées à l'emplacement des fichiers originaux.
 
+Exemple de gain d'espace de stockage attendu : environ 20 à 25% pour un fichier texte au format odt, doc, docx, txt... compressé en .zip.
 
 ## Table des matières :  
 - [Fonctionnement:](#Fonctionnement)
+- [Installation:](#Installation)
 - [Utilisation:](#Utilisation)
 - [Prerequis:](#Prerequis)
 - [Version:](#Version)
 - [License:](#License)
-- [...]
+
 
 ## Fonctionnement
-Fonctionnement [...]
+* Le module utilise comme premier argument le chemin pointant vers le répertoire à traiter (via la variable ***path***). 
+* Il utilise comme second argument l'intervalle de temps en secondes jusqu’à la date limite cible (via la variable ***timedelt***).
+* Le module balaye les fichiers et tous les sous-répertoires du répertoire cible (traitement fonctionnel récursif)
+* Il exclu du balayage et de l'archivage / compression les fichiers aux formats choisis comme devant être exclus. Sont exclus à titre d'exemple dans notre cas les fichiers iso et zip. 
+* Il compare la date de dernière consultation des fichiers avec la date limite cible. 
+* Il archive / compresse au format .zip les fichiers dont la  dernière consultation est antérieure à la date choisie.
+* Il supprime de l'espace de stockage les fichiers originaux après leur archivage / compression.
+* Il laisse les fichiers archivés à l'emplacement des fichiers originaux.
+
+
+## Installation
+Pour installer les fichiers du projet, voici 2 options :
+1. **Copie des fichiers:**
+Téléchargez l'archive des éléments Ansible du projet en cliquant sur ce [lien](https://github.com/guillerbe/Archive_old_files/archive/refs/heads/dev.zip) . Décompréssez l'archive dans votre espace de travail Ansible.
+2. **Clonage Git du projet:**
+Placez vous dans le répertoire que vous aurez choisi, par exemple dans le répertoire temporaire puis tapez la commande suivante : 
+```
+cd /tmp/
+
+git clone https://github.com/guillerbe/Archive_old_files.git  
+```  
 
 ## Utilisation
-Utilisation [...]
+* Accédez à l'espace de travail Ansible, l'espace où se situe les fichiers d'inventaire, le fichier playbook et le répertoire rôles. Ce dernier est composé du sous dossiers ***library*** qui contient le module ***module_archive_old_files.py*** et du sous dossier ***tasks*** qui contient le fichier d'instructions ***main.yml***. 
+* Le fichier d'inventaire se nomme ***00_inventory.yml***. Editez le en entrant : le nom du groupe ou des groupes de machines et le nom des machines.
+```
+vim 00_inventory.yml
+```  
 
+```  
+all:
+  children:
+    fservers:
+      hosts:
+        ubsfs01:
+        ubdit01:
+```  
+* Le fichier playbook se nomme ***playbook_roles.yml***. Assurez-vous que les noms : des machines, de l'utilisateur Ansible et du rôle ''appelé'' correspondent aux configurations attendues. 
+```
+vim playbook_roles.yml
+```  
+```
+---
+- name: Mon playbook archive old files
+  hosts: all 
+  remote_user: user-ansible
+  become_user: yes
+  roles:
+  - archive_old_files
+```
+
+* Les arguments utilisés par le module se nomment ***path*** et ***timedelt***. Leur valeur est affectée aux variables de mêmes nom : ***path*** et ***timedelt***. Assurez-vous de ces valeurs  dans le fichier ***main.yml*** du sous répertoire ***"tasks"***  contenu dans le répertoire du rôle dédié au projet: ***roles/archive_old_files/tasks/main.yml*** .  
+```
+vim roles/archive_old_files/tasks/main.yml
+```  
+```  
+---
+- name: module_archive_old_files
+  module_archive_old_files:
+    path: "/home/guillerbe/Shared"
+    timedelt: 3
+```  
+* L'unité de temps de l'intervalle jusqu'à la date limite de dernière consultation des fichiers est en secondes. C'est donc une **valeur en secondes** qui devra être affectée à la variable ***timedelt:***
+
+* Exécuter le playbook avec la commande Ansible suivante :
+```
+ansible-playbook -i 00_inventory.yml -k playbook_roles.yml  
+```    
 ## Prerequis :  
-* Le service Ansible doit être installé sur un ordinateur.
-* Cette machine communique en ssh avec les machines cibles.
-
-
+* Un serveur node contrôleur (ou node manager) Ansible est en service.
+* Ce serveur communique en ssh avec les machines cibles (avec les nodes managés).
+* Toutes les machines disposent d'un compte utilisateur ***"user-ansible"*** configuré avec les privilèges sudo.
+* Toutes les machines sont déclarées dans le fichier ***"/etc/hosts"*** des autres machines si pas de résolution de nom géré par un serveur DNS.  
+  
 ## Version :  
-0.2.5
+0.3.0
 
 ## License :  
 #### GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
--- roles/archive_old_files/library/module_archive_old_files.py
+++ roles/archive_old_files/library/module_archive_old_files.py
@@ -12,107 +12,76 @@ from os import listdir
 from os.path import isfile, join, getatime
 from datetime import datetime, timedelta
 
-def function_archive_old_files(path, time_up_to_deadline = 3650):
+def function_archive_old_files(path, time_up_to_deadline = 31536000):
+    # counter : Compteur qui sert à indiquer un changement dans le 
+    # PLAY RECAP lors de l'exécution du playbook.
+    # Quand il y a archivage / compression de fichier(s), l'indicateur 
+    # "changed" passe de 0 à 1.
     counter = 0
     # Boucle de génération de la listes des fichiers
     for file in os.listdir(path):
         # Définition variable f : file. 
-        # f : concaténation chemin absolue et nom de fichiers (join : concatenation)
+        # f : concaténation chemin absolu et nom de fichiers (join : concatenation)
         f = os.path.join(path, file)
-        # Condition de génération de la liste des fichiers 
+        # Condition de génération de la liste des fichiers + amorçage du compteur.
         if os.path.isdir(f):
             counter = counter + function_archive_old_files(f, time_up_to_deadline)
         if os.path.isfile(f):
-            # -----------------------------------------------
+            # ----------------------------------------------------
             # Exclusion du traitement : ex > zip, iso, ...
             # re : Regular Expression - search : recherche dans toute la chaine
-            # re.search(pattern, str) - je ne trouve pas de solution pour recherche 
-            # de plusieurs patterns dans la même ligne
-            exc1 = re.search(".*\.zip$", f)
-            exc2 = re.search(".*\.iso$", f)
-            # if (cond1 AND/OR COND2) AND/OR (cond3 AND/OR cond4):
-            #if (exc1) or (exc2):
-            if (not (exc1)) and (not (exc2)):
-                # Si besoin affichage détection fichiers ZIP ou ISO.
-                # Décommenter la ligne ci-dessous :
-                #print("PAS FICHIER ZIP ou ISO")
+            # Structure : re.search(pattern, str) 
+            exclu1 = re.search(".*\.zip$", f)
+            exclu2 = re.search(".*\.iso$", f)
+            # Condition de conjonction de négation pour assurer l'exclusion des éléments 
+            # (permet éviter la disjonction du "ou").
+            # Exemple de strucure d'encadrement entre parenthèse : 
+            # if (cond1 AND/OR COND2) AND/OR (cond3 AND/OR cond4).
+            if (not (exclu1)) and (not (exclu2)):
                 # ------------------------------------------------
-                # Affichage des fichiers cible en chemin absolu depuis racine
-                ## => print(f)
-                # Déf variable : time_up_to_deadline : durée jusqu'à la date limite
-                # La date lim étant la date en deça ou au dessus de laquelle l'action est effectuée.
-                # # # time_up_to_deadline = 7
                 # Date actuelle au format datetime : 
-                # ex : 2021-05-10 12:32            function_archive_old_files(f):11.503663
+                # ex : 2021-05-10 12:32            
                 dat_now = datetime.now()
-                # Affichage de la date date locale actuelle format datetime
-                ## => print("Date actuelle :  ", dat_now)
-                # Déf variable : unité de temps : correspond à l'unité de temps entre "maintenant" et la 
-                # date lim. cible : weeks ou days, hours, minutes, seconds, microseconds, milliseconds.
-                # ex : unit_of_time = weeks
-                # Déf variable : date lim cible en deça ou au dessus de laquelle l'action est effectuée. 
+                # Déf variable date lim cible : date en deça ou au dessus de laquelle l'action est effectuée. 
                 # timedelta : intervalle de temps soustrait à la date actuelle.
                 # class datetime.timedelta(days=0, seconds=0, microseconds=0, 
                 # milliseconds=0, minutes=0, hours=0, weeks=0)
-                ###dat_lim_c = dat_now - timedelta(days = time_up_to_deadline)
                 dat_lim_c = dat_now - time_up_to_deadline
-                # Affichage de la date limite cible 
-                ## => print("Date lim. cible :", dat_lim_c)
                 # Conversion date limite en seconde = format "epoch"
                 dat_lim_c_in_sec = dat_lim_c.timestamp()
-                # Si besoin affichage date limite en seconde = format "epoch"
-                # décommenter ligne ci-dessous :            
-                ## => print("Date limite epoch :  ", dat_lim_c_in_sec)  
                 # Définition de la variable de date de dernière consultation format epoch
                 access_time = getatime(f)
-                # Si besoin d'afficher dernière date d'accès en sec depuis "epoch"
-                # décommenter la ligne => print [...] access_time, ci-dessous
-                ## => print("Der. consult. epoch :", access_time)
-                # Définition de la variable date de dernière consultation 
-                # en temps local au format ctime 
-                # ex : Thu Apr 29 15:26:26 2021
-                local_time = time.ctime(access_time)
-                # Affichage de la date locale de dernière consultation en format ctime 
-                # ex : Thu Apr 29 15:26:26 2021
-                ## => print("Der. consult. :", local_time)
                 # Conditions déclenchant les actions :
                 # Cond 1 : si date de der. consult. dépasse (antérieur) à date limite 
                 if access_time < dat_lim_c_in_sec: # Renvoi True ou False 
-                    ## => print("Der. consult. du fichier antérieur à date limite :", access_time < dat_lim_c_in_sec # Renvoi True ou False
-                    ## => print(">>>>> A archiver ! <<<<<" + '\n') 
                     # ----------------------------------------------
                     # Archivage & compression : module shutil
                     # base_dir=os.path.basename(f) : permet d'archiver / compresser que le fichier seul (sans l'arbo)
                     shutil.make_archive(base_dir=os.path.basename(f), root_dir=path + '/', format='zip', base_name=f)
                     # ----------------------------------------------
-                    # Suppression du fichier original (non archivé / compréssé)              
+                    # Suppression du fichier original (du fichier non archivé / compréssé)              
                     os.remove(f)
                     # -------------------------------------------------
-                    # Cond 2 : autrement si date de der. consult. pas antérieur à la date lim 
-                    # elif access_time > dat_lim_c_in_sec:
-                    ## => print("Der. consult. du fichier antérieur à date limite :", access_time < dat_lim_c_in_sec) # Renvoi True ou False
-                    ## => print(">>>>> Ne pas archiver ! <<<<<" + '\n')
-                    # ++ correspond à counter = counter +1
+                    # Incrémentation de +1 du compteur
                     counter = counter + 1
+                    
     return counter
 
 def main():
     module = AnsibleModule( 
         argument_spec = dict(
             path = dict(required=True, type='str'), 
-            timedelt = dict(type='int', default = 7, aliases=['interval']) 
+            timedelt = dict(type='int', default = 7) 
         )
     )
-    path = module.params['path']
-    #timedelt = module.params['timedelt']
-    interval = timedelta(days=module.params['timedelt'])
-    #timedelt = timedelta(days=module.params['timedelt'])
-    nbfichiers = function_archive_old_files(path, interval)
-    test = False
-    if nbfichiers > 0:
-        test = True
-    module.exit_json(changed=test)
+    chemin = module.params['path']
+    interval = timedelta(seconds=module.params['timedelt'])
 
+    nbfichiers = function_archive_old_files(chemin, interval)
+    result = False
+    if nbfichiers > 0:
+        result = True
+    module.exit_json(changed=result)
 
 if __name__ == '__main__':
     main()
